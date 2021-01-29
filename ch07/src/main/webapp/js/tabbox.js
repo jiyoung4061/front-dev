2ad5716956f743572739ac41548f17ce6b8de8d7
jiyoung <jiyoung4061@naver.com>
2021-01-29 15:30:30 +0900

    jQuery 연습 #5

---
+++ ch07/src/main/webapp/js/tabbox.js
@@ -0,0 +1,20 @@
+// jQuery Based
+const TabBox = {	
+	init: function(){
+		$(this._init);
+	},
+	_init: function(){
+		$('.tab-box li').click(TabBox._changeTab);
+		TabBox._changeTab();
+	},
+	_changeTab: function(){
+		// unselect
+		$('.tab-box li.selected').removeClass('selected');
+		
+		// select
+		let $liTab = (this == TabBox) ? $('.tab-box li:last-child') : $(this);
+		
+		$('.tab-box div').text($liTab.data('no') + "의 탭뷰입니다");
+		$liTab.addClass('selected');
+	}
+}
